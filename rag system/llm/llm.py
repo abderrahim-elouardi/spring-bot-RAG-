78096eddc4c32283cf0f21e6d6f677b032163a20
abderrahim-elouardi <elouardiabderrahim06@gmil.com>
2026-05-07 17:45:33 +0100

    separing task (generation , making prompt , retrievling)

--- rag system/llm/llm.py
+++ rag system/llm/llm.py
@@ -1,3 +1,18 @@
 from langchain_ollama import ChatOllama
+from openai import AsyncOpenAI
+import yaml
+import yaml
 
 llm = ChatOllama(model="deepseek-r1", temperature=0)
+
+file_path = "..\\config\\setting.yaml"
+key_path = ["llm", "key"]
+with open(file_path, 'r', encoding='utf-8') as f:
+    data = yaml.safe_load(f)
+    # Navigation dynamique dans les clés
+    # Si key_path est ["vector_store", "path"]
+    key = data.get("llm", {}).get("key", "Clé introuvable")
+    client = AsyncOpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=key,
+    )
